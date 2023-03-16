35dfc2e2c32e25fa63d6f6b561010957ec631756
Nahyun Park <nahowo@naver.com>
2023-03-16 21:16:00 +0900

    [백준] 피보나치 수 5

--- 10870.py
+++ 10870.py
@@ -3,7 +3,7 @@ sys.setrecursionlimit(10**6)
 n=int(input())
 def fibo(a,b,count):
     if count==n:
-        print(b)
+        print(a)
         return 0
     fibo(b,a+b,count+1)
-fibo(0,1,1)
+fibo(0,1,0)
