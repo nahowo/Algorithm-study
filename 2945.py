711d0cea3195432e042f69dfd2759eebf38f5cc8
Nahyun Park <nahowo@naver.com>
2023-03-21 14:22:07 +0900

    [백준] 퍼거슨과 사과

--- 2945.py
+++ 2945.py
@@ -4,7 +4,7 @@ gcd_=math.gcd(r,g)
 tmp=int((gcd_)**(1/2))
 num=[]
 for i in range(1,tmp+1):
-    if r%i==0 and g%i==0:
+    if gcd_%i==0:
         num.append(i)
         if i**2!=gcd_:
             num.append(gcd_//i)
