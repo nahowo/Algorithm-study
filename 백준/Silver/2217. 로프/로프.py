78737aee093eb918473da14e399cd626ddc9d979
Nahyun Park <nahowo@naver.com>
2023-06-03 12:52:23 +0900

    [Silver IV] Title: 로프, Time: 152 ms, Memory: 35500 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+sum_=0
+answer=0
+rope=[int(input()) for _ in range(n)]
+rope.sort(reverse=True)
+for i in range(1,n+1):
+    sum_+=rope[i-1]
+    tmp=sum_/i
+    if tmp>rope[i-1]:
+        sum_-=rope[i-1]
+    if rope[i-1]*i>answer:
+        answer=rope[i-1]*i
+print(answer)
