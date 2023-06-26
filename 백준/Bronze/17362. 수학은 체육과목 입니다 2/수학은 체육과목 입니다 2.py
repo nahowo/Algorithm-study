801acc58fa9de70c76ad4878619ac01d08094eac
Nahyun Park <nahowo@naver.com>
2023-06-26 22:01:43 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+n=n%8
+if n==1:
+    print(1)
+elif n==2 or n==0:
+    print(2)
+elif n==3 or n==7:
+    print(3)
+elif n==4 or n==6:
+    print(4)
+else:
+    print(5)
