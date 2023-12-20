b9fe8deba38f7298a86fd4f5d38361ef83357b69
Nahyun Park <nahowo@naver.com>
2023-12-20 16:47:07 +0900

    Time: 66 ms (61.47%), Space: 17.5 MB (46.08%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d=dict()
-        s=set(nums)
         for i in range(len(nums)):
             d[nums[i]]=i
         for i in range(len(nums)):
-            if target-nums[i] in s and d[target-nums[i]]!=i:
+            if target-nums[i] in d and d[target-nums[i]]!=i:
                 return [d[target-nums[i]],i]
