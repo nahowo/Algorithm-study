b524f82c9b77bd67f4397b877565913128307b79
Nahyun Park <nahowo@naver.com>
2023-12-21 22:17:39 +0900

    Time: 198 ms (5.70%), Space: 14.4 MB (70.72%) - LeetHub

---
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        s=0
+        e=len(nums)-1
+        while s<=e:
+            m=(s+e)//2
+            if nums[m]==target:
+                return m
+            elif nums[m]>target:
+                e=m-1
+            else:
+                s=m+1
+        return -1
