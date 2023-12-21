bf141441def53b80e00099fdc5ab5027eea85fff
Nahyun Park <nahowo@naver.com>
2023-12-21 23:33:31 +0900

    Time: 16 ms (49.72%), Space: 13.3 MB (87.15%) - LeetHub

--- LeetCode/Easy/0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ LeetCode/Easy/0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -5,24 +5,18 @@ class Solution(object):
         :type s2: str
         :rtype: List[str]
         """
-        d1=dict()
-        d2=dict()
+        d=dict()
         s1=s1.split()
         s2=s2.split()
         uncommon=[]
         
         for i in range(len(s1)):
-            d1[s1[i]]=d1.get(s1[i],0)+1
+            d[s1[i]]=d.get(s1[i],0)+1
         for i in range(len(s2)):
-            d2[s2[i]]=d2.get(s2[i],0)+1
+            d[s2[i]]=d.get(s2[i],0)+1
         
-        for i,j in d1.items():
+        for i,j in d.items():
             if j==1:
-                if i not in d2:
-                    uncommon.append(i)
-        for i,j in d2.items():
-            if j==1:
-                if i not in d1:
-                    uncommon.append(i)
+                uncommon.append(i)
         
         return uncommon
