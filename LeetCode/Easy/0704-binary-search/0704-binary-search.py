ebe0fa1a28bbb12a33290db6854cfbb68a5ff03a
Nahyun Park <nahowo@naver.com>
2023-12-21 22:24:43 +0900

    Time: 179 ms (61.56%), Space: 14.5 MB (39.77%) - LeetHub

--- LeetCode/Easy/0704-binary-search/0704-binary-search.py
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.py
@@ -9,10 +9,10 @@ class Solution(object):
         e=len(nums)-1
         while s<=e:
             m=(s+e)//2
-            if nums[m]==target:
-                return m
-            elif nums[m]>target:
+            if nums[m]>target:
                 e=m-1
-            else:
+            elif nums[m]<target:
                 s=m+1
+            else:
+                return m
         return -1
