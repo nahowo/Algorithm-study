9553bb0a5c61ed42f6d3dc3b8bf9cec907ae12b1
Nahyun Park <nahowo@naver.com>
2024-01-13 11:58:42 +0900

    [Bronze IV] Title: 나이 계산하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16199. 나이 계산하기/나이 계산하기.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline
+
+by,bm,bd=map(int,input().split())
+ty,tm,td=map(int,input().split())
+
+if tm>bm or (tm==bm and td>=bd):
+    year1=ty-by
+else:
+    year1=max((ty-by-1),0)
+
+year2=ty-by+1
+year3=ty-by
+
+print(year1)
+print(year2)
+print(year3)
