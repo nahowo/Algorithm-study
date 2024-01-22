bcab34d6d4393d65074fcd76a180d7b9eb5901e7
Nahyun Park <nahowo@naver.com>
2024-01-22 11:59:15 +0900

    [Bronze II] Title: 노브 돌리기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/23756. 노브 돌리기/노브 돌리기.py
+++ 백준/Bronze/23756. 노브 돌리기/노브 돌리기.py
@@ -7,9 +7,7 @@ angle=0
 
 for i in range(n):
     tmp=int(input())
-    if tmp>=a:
-        angle+=min(tmp-a,a+360-tmp)
-    else:
-        angle+=min(a-tmp,tmp+360-a)
+    a=abs(a-tmp)
+    angle+=min(a,360-a)
     a=tmp
 print(angle)
