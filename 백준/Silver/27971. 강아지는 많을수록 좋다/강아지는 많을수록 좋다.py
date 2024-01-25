c678386f5bc39b94ef3369130e6d0e9966d5caae
Nahyun Park <nahowo@naver.com>
2024-01-25 14:00:47 +0900

    [Silver I] Title: 강아지는 많을수록 좋다, Time: 300 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
@@ -0,0 +1,47 @@
+import sys
+input=sys.stdin.readline
+
+def func():
+    n,m,a,b=map(int,input().split())
+    if a>b:
+        a,b=b,a
+    dog=[0]*(n+1)
+    dog[0]=0
+
+    for _ in range(m):
+        op,cl=map(int,input().split())
+        for i in range(op,cl+1):
+            dog[i]=-1
+
+    for i in range(1,n+1):
+        if dog[i]==-1:
+            continue
+        if i<a:
+            dog[i]=-1
+        elif i<b:
+            if dog[i-a]==-1:
+                dog[i]=-1
+            else:
+                dog[i]=dog[i-a]+1
+
+        elif dog[i-a]==-1 and dog[i-b]==-1:
+            dog[i]=-1
+        elif dog[i-a]==-1:
+            dog[i]=dog[i-b]+1
+        elif dog[i-b]==-1:
+            dog[i]=dog[i-a]+1
+        else:
+            dog[i]=min(dog[i-a],dog[i-b])+1
+        # else:
+        #     tmplist=[]
+        #     for j in [a,b]:
+        #         if dog[i-j]!=False:
+        #             tmplist.append(dog[i-j]+1)
+        #     if len(tmplist)<1:
+        #         dog[i]=False
+        #     else:
+        #         dog[i]=min(tmplist)
+    # return dog[-1] if dog[-1]!=False else -1
+    return dog[n]
+
+print(func())
