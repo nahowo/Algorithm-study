45580b296018c6c9eda59b46e20514ed6f18f43a
Nahyun Park <nahowo@naver.com>
2024-01-30 12:02:37 +0900

    [Silver I] Title: 강아지는 많을수록 좋다, Time: 168 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
+++ 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
@@ -3,35 +3,22 @@ input=sys.stdin.readline
 
 def func():
     n,m,a,b=map(int,input().split())
-    if a>b:
-        a,b=b,a
-    dog=[0]*(n+1)
-    dog[0]=0
+    dog=[0]+[-1]*n
 
     for _ in range(m):
         op,cl=map(int,input().split())
-        for i in range(op,cl+1):
-            dog[i]=-1
+        dog[op:cl+1]=[-2]*(cl-op+1)
 
-    for i in range(1,n+1):
-        if dog[i]==-1:
-            continue
-        if i<a:
-            dog[i]=-1
-        elif i<b:
-            if dog[i-a]==-1:
-                dog[i]=-1
+    for i in range(min(a,b),n+1):
+        if dog[i]!=-2:
+            if dog[i-a]<0 or dog[i-b]<0:
+                if dog[i-a]>=0:
+                    dog[i]=dog[i-a]+1
+                if dog[i-b]>=0:
+                    dog[i]=dog[i-b]+1
             else:
-                dog[i]=dog[i-a]+1
-    
-        elif dog[i-a]==-1 and dog[i-b]==-1:
-            dog[i]=-1
-        elif dog[i-a]==-1:
-            dog[i]=dog[i-b]+1
-        elif dog[i-b]==-1:
-            dog[i]=dog[i-a]+1
-        else:
-            dog[i]=min(dog[i-a],dog[i-b])+1
-    return dog[n]
+                dog[i]=min(dog[i-a],dog[i-b])+1
+
+    return dog[-1] if dog[-1]>0 else -1
 
 print(func())
