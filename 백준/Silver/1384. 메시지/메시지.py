7fa634bd726d5c5257fea7e67a7d282fde113e26
Nahyun Park <nahowo@naver.com>
2024-02-06 00:35:36 +0900

    [Silver V] Title: 메시지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1384. 메시지/메시지.py
+++ 백준/Silver/1384. 메시지/메시지.py
@@ -8,18 +8,15 @@ while True:
         break
     kids=[]
     bad_words=[]
-    flag=False
     for i in range(n):
         tmp=list(input().rstrip().split())
         kids.append(tmp[0])
-        if tmp[1:].count("N")>0:
-            flag=True
-            for j in range(1,n):
-                if tmp[j]=="N":
-                    bad_words.append([(i-j)%n,i])
+        for j in range(1,n):
+            if tmp[j]=="N":
+                bad_words.append([(i-j)%n,i])
 
     print("Group "+str(g))
-    if not flag:
+    if len(bad_words)==0:
         print("Nobody was nasty")
     else:
         for i in bad_words:
