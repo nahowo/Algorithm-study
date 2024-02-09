2f580fe12d4fcbc0c89936aa3de31fbc74474281
Nahyun Park <nahowo@naver.com>
2024-02-09 16:10:51 +0900

    [Silver IV] Title: 셔틀런, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/13268. 셔틀런/셔틀런.py
+++ 백준/Silver/13268. 셔틀런/셔틀런.py
@@ -1,25 +1,32 @@
-n = int(input())
-d = 0
+import sys
+input=sys.stdin.readline
 
-while(n > 0):
-    d = 0
-    for i in 1,2,3,4:
-        for j in range(i):
-            n -= 5
-            if n <= 0:
-                d += 5+n
-                break
-            d += 5
-        if n <= 0:
-            break
-            
-        for j in range(i):
-            n -= 5
-            if n <= 0:
-                d -= 5+n
-                break
-            d -= 5
-        if n <= 0:
-            break
+def func():
+    stamina=int(input())%100
 
-print(d//5 if d%5 == 0 else d//5+1)
+    zone=0
+    for i in range(1,5): # 콘의 개수만큼 반복
+        for k in range(2): # 왕복 2번
+            for j in range(i): # 콘마다 왕복
+                if stamina>=5: # 다음 콘으로 이동 가능한 경우
+                    if k==0:
+                        zone+=1
+                    else:
+                        zone-=1
+                    stamina-=5
+
+                if stamina<5: # 다음 칸으로 이동할 수 없는 경우
+                    if stamina==0: # 아예 이동할 수 없는 경우
+                        return zone
+                    else:
+                        if zone==0: # 시작점에서 다시 돌아가야 하는 경우
+                            return zone+1
+                        elif zone==i: # 맨 끝 콘에서 다시 돌아가야 하는 경우
+                            return zone
+                        else: # 중간 콘일 경우
+                            if k==0: # 더 앞으로 가야 하는 경우
+                                return zone+1
+                            else: # 더 뒤로 가야 하는 경우
+                                return zone
+
+print(func())
