8cd3649d37abaa36a8da3419fffe19564f73090b
Nahyun Park <nahowo@naver.com>
2024-02-19 13:09:49 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.py
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.py
@@ -1,17 +1,22 @@
 import sys
 input=sys.stdin.readline
-
+    
 def func():
     n=int(input())
     doll=list(map(int,input().split()))
+    d=dict()
+    s=set(doll)
     answer=0
-
-    while len(doll)>0:
-        tmp=set(doll)
-        for i in tmp:
-            del doll[doll.index(i)]
-        answer+=1
-
-    return answer
+    for i in doll:
+        d[i]=d.get(i,0)+1
     
+    while d:
+        for i in s:
+            if d.get(i)!=None:
+                d[i]-=1
+                if d[i]==0:
+                    del d[i]
+        answer+=1
+    return answer
+
 print(func())
