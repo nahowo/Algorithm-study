e7de88ac66c6cc91ea8f9f8e0d33f22ff74b977f
Nahyun Park <nahowo@naver.com>
2024-02-19 22:12:10 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.py
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.py
@@ -5,18 +5,11 @@ def func():
     n=int(input())
     doll=list(map(int,input().split()))
     d=dict()
-    s=set(doll)
     answer=0
     for i in doll:
         d[i]=d.get(i,0)+1
-    
-    while d:
-        for i in s:
-            if d.get(i)!=None:
-                d[i]-=1
-                if d[i]==0:
-                    del d[i]
-        answer+=1
+        answer=max(answer,d.get(i))
+
     return answer
 
 print(func())
