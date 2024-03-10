9afa11ab4652548eef8d163dcc2c42a9037b6956
Nahyun Park <nahowo@naver.com>
2024-03-10 12:06:23 +0900

    Time: 25 ms (98.63%), Space: 16.5 MB (86.78%) - LeetHub

--- LeetCode/Easy/0066-plus-one/0066-plus-one.py
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.py
@@ -1,13 +1,5 @@
 class Solution:      
     def plusOne(self, digits: List[int]) -> List[int]:
-        i=len(digits)-1
-        while i>-1:
-            if digits[i]==9:
-                digits[i]=0
-                i-=1
-            else:
-                digits[i]+=1
-                break
-        if digits[0]==0:
-            digits.insert(0,1)
+        strint=str(int(''.join(map(str,digits)))+1)
+        digits=list(map(int,list(strint)))
         return digits
