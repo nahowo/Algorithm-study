94a0b06ddb26f7cca6685b2bcdad2f5fe627e39f
Nahyun Park <nahowo@naver.com>
2024-03-19 18:21:21 +0900

    Time: 451 ms (78.22%), Space: 30 MB (36.48%) - LeetHub

--- LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,20 +1,11 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        
-        # solution without sorting
-        
-        import heapq
-        
-        for i in range(len(nums)): # O(n)
-            nums[i]=-nums[i]
-        heapq.heapify(nums) # O(n)
-        for i in range(k): # O(n)
-            kth=heapq.heappop(nums) # O(logn)
-        return -kth
-        
-        # O(nlogn) + O(n) + O(n)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        l=10**4
+        arr=[0]*((l*2)+1)
+        for i in nums:
+            arr[i+l]+=1 # 음수 인덱스 바이어스 처리
+            
+        for i in range(len(arr)-1,-1,-1):
+            k-=arr[i]
+            if k<=0:
+                return i-l
