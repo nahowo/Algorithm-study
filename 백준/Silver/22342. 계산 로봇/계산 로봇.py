3b22ea9e7005cd11bfa86d01f5bf1015dc49a948
Nahyun Park <nahowo@naver.com>
2024-04-10 22:00:17 +0900

    [Silver I] Title: 계산 로봇, Time: 48 ms, Memory: 31120 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -0,0 +1,35 @@
+import sys
+input=sys.stdin.readline
+
+def func():
+    m,n=map(int,input().split())
+    maxSave=0
+
+    # 가중치
+    d=[]
+    for _ in range(m):
+        d.append(list(map(int,list(input().rstrip()))))
+    
+    # 저장값
+    save=[[0]*n for _ in range(m)]
+    for i in range(m):
+        save[i][0]=d[i][0]
+    
+    # 출력값
+    output=[[0]*n for _ in range(m)]
+    for i in range(m):
+        output[i][0]=d[i][0]
+    
+    # 출력값들 중 최대를 저장값에 저장(저장값 최대 업데이트), 저장값+가중치를 출력값에 저장
+    for j in range(1,n): # 열
+        for i in range(m): # 행
+            tmp=[]
+            for k in range(i-1,i+2):
+                if 0<=k<m:
+                    tmp.append(output[j-1][k])
+            save[i][j]=max(tmp)              # 저장값 계산
+            maxSave=max(maxSave,save[i][j])  # 최대값 업데이트
+            output[i][j]=save[i][j]+d[i][j]
+    return maxSave
+
+print(func())
