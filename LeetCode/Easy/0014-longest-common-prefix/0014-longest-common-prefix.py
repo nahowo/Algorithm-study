6c1557f96424dc9c5109a35adf6ea931546892b7
Nahyun Park <nahowo@naver.com>
2024-04-15 01:15:07 +0900

    Time: 29 ms (95.16%), Space: 16.5 MB (90.15%) - LeetHub

---
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        answer=""
+        n=len(strs)
+        m=200
+        for i in strs:
+            m=min(m,len(i))
+            
+        for j in range(m):
+            cnt=0
+            for i in range(n):
+                if strs[0][j]==strs[i][j]:
+                    cnt+=1
+            if cnt==n:
+                answer+=strs[0][j]
+            else:
+                return answer
+        return answer
