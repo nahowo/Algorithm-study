e914cd66383ff4ddf2d52e562ee2f8eb03ab17af
Nahyun Park <nahowo@naver.com>
2024-04-15 09:44:40 +0900

    Time: 130 ms (26.02%), Space: 20.3 MB (95.76%) - LeetHub

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.py
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.py
@@ -1,28 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        d=dict()
-        for i in range(len(intervals)):
-            d[i+1]=intervals[i]
-        
-        nums=[0]*(10001)
-        for i in range(len(intervals)):
-            s=i+1
-            for j in range(intervals[i][0],intervals[i][1]+1):
-                if nums[j]!=0:
-                    k=nums[j]
-                    s=k
-                    d[k]=[min(d[k][0],intervals[i][0]),max(d[k][1],intervals[i][1])]
-            for j in range(intervals[i][0],intervals[i][1]+1):
-                nums[j]=s
-        
-        nums.append(0)
-        answer=[]
-        k=0
-        for i in range(10002):
-            if nums[k]==0 and nums[i]!=0:
-                k=i
-            elif nums[k]!=nums[i]:
-                answer.append([k,i-1])
-                k=i
-        return answer
+        intervals.sort()
+
+        stack=[]
+        for i in intervals:
+            if len(stack)==0:
+                stack.append(i)
+            else:
+                if stack[-1][1]>=i[0]:
+                    stack[-1]=[stack[-1][0],max(stack[-1][1],i[1])]
+                else:
+                    stack.append(i)
+        return stack
