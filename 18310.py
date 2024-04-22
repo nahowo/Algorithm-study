239f3101c825c011fffc21b55da2e5d06a625edd
nahowo <nahowo@naver.com>
2024-04-23 00:09:36 +0900

    fix: 디렉토리 정리

--- 18310.py
+++
@@ -1,9 +0,0 @@
-import sys
-input=sys.stdin.readline
-n=int(input())
-house=sorted(list(map(int,input().split())))
-if n%2==0:
-    answer=house[n//2-1]
-else:
-    answer=house[n//2]
-print(answer)
--- 27866.py
+++
@@ -1,5 +0,0 @@
-import sys
-input=sys.stdin.readline
-word=input()
-index=int(input())
-print(word[index-1])
