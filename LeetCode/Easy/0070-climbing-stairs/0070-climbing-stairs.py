5c886fc0cb0581763ab3f508b5947f1c519a0268
Nahyun Park <nahowo@naver.com>
2024-05-07 10:28:11 +0900

    Time: 29 ms (87.46%), Space: 16.5 MB (83.9%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n<3:
+        if n < 3:
             return n
-        
         dp=[0]*(n+1)
         dp[1]=1
         dp[2]=2
