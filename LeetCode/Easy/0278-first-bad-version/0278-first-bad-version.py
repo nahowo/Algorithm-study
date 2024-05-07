c0fe2f5f76bd44d5b7bcd20d0e9331055206bf68
Nahyun Park <nahowo@naver.com>
2024-05-07 21:33:34 +0900

    Time: 40 ms (17.29%), Space: 16.6 MB (12.45%) - LeetHub

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.py
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.py
@@ -3,16 +3,16 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        s=1
-        e=n
-        
-        answer=n
+        s =1
+        e =n
+        answer =n
 
         while s<=e:
-            m=int((s+e)/2)
+            m = int((s+e)/2)
             if not isBadVersion(m):
                 s=m+1
             else:
                 e=m-1
                 answer=m
+                
         return answer
