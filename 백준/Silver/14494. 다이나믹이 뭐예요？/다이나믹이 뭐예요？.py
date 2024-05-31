4e34d80aa021fc7ce77e3f107f3413afe9da35d4
Nahyun Park <nahowo@naver.com>
2024-05-31 16:45:55 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 272 ms, Memory: 70040 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -4,16 +4,13 @@ DIV=1000000007
 
 def func():
     n,m=map(int,input().split())
-    dp=[[0]*(n) for _ in range(m)]
-    for i in range(n):
-        dp[0][i]=1
-    for i in range(m):
-        dp[i][0]=1
+    dp=[[0]*(n+1) for _ in range(m+1)]
+    dp[0][0]=1
 
-    for i in range(1,m):
-        for j in range(1,n):
+    for i in range(1,m+1):
+        for j in range(1,n+1):
             dp[i][j]=(dp[i-1][j]+dp[i][j-1]+dp[i-1][j-1])%DIV
 
-    return dp[m-1][n-1]
+    return dp[m][n]
 
 print(func())
