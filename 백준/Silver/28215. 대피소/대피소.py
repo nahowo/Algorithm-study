aed3b10975b6d9ae37d414f5d686501c8159d291
Nahyun Park <nahowo@naver.com>
2024-07-12 02:21:31 +0200

    [Silver IV] Title: 대피소, Time: 276 ms, Memory: 113772 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -1,35 +1,5 @@
-# import sys
-# input = sys.stdin.readline
-# from itertools import combinations
-
-# def distance(a, b):
-#     return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-# def solution(combi):
-#     tmpMax = 0
-#     for i in range(n):
-#         tmpMin = sys.maxsize
-#         for j in combi:
-#             dist = distance(houese[i], houese[j])
-#             tmpMin = min(dist, tmpMin)
-#         tmpMax = max(tmpMin, tmpMax)
-#     return tmpMax
-
-
-# n, k = map(int,input().split())
-# houese = []
-# for i in range(n):
-#     houese.append(list(map(int, input().split())))
-
-# maxLength = sys.maxsize
-# for combi in combinations(range(n), k):
-#     maxLength = min(maxLength, solution(combi))
-
-# print(maxLength)
-
 import sys
 input = sys.stdin.readline
-from itertools import combinations
 
 def distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
@@ -54,7 +24,6 @@ def solution(combi):
             tmpMin = min(dist, tmpMin)
         tmpMax = max(tmpMin, tmpMax)
     return tmpMax
-
 
 n, k = map(int,input().split())
 houese = []
