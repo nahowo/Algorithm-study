12aaae7b7c5dadabd1c27b618fc505e9c52539f1
Nahyun Park <nahowo@naver.com>
2024-08-07 12:28:55 +0900

    [Gold V] Title: LCS, Time: 276 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,19 +1,20 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-def func():
-    s1=' '+input().rstrip()
-    s2=' '+input().rstrip()
-    n,m=len(s1),len(s2)
-    dp=[[0]*n for _ in range(m)]
+def solution():
+    s1 = ' ' + input().rstrip()
+    s2 = ' ' + input().rstrip()
+    l1 = len(s1)
+    l2 = len(s2)
 
-    for i in range(1,m):
-        for j in range(1,n):
-            if s1[j]==s2[i]:
-                dp[i][j]=dp[i-1][j-1]+1
+    dp = [[0] * (l1) for _ in range(l2)]
+    for i in range(1, l2):
+        for j in range(1, l1):
+            if s2[i] == s1[j]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
             else:
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    
+    return dp[l2 - 1][l1 - 1]
 
-    return dp[m-1][n-1]
-
-print(func())
+print(solution())
