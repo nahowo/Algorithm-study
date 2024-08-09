1d8bb8bb4bc44dfd3d87e41fc27251bfba213952
Nahyun Park <nahowo@naver.com>
2024-08-09 10:43:23 +0900

    [Silver III] Title: 배열 복원하기, Time: 168 ms, Memory: 45748 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    h, w, x, y = map(int, input().split())
+    bTable = [[0] * (w + 2 * y) for _ in range(h + 2 * x)]
+    for i in range(h + x):
+        tmp = list(map(int, input().split()))
+        for j in range(w + y):
+            bTable[i + x][j + y] = tmp[j]
+
+    aTable = [[0] * (w + y) for _ in range(h + x)]
+    for i in range(x, h + x):
+        for j in range(y, w + y):
+            aTable[i][j] = bTable[i][j] - aTable[i - x][j - y]
+
+    answer = []
+    for i in range(x, h + x):
+        answer.append(aTable[i][y : w + y])
+    return answer
+
+result = solution()
+for i in result:
+    print(*i)
