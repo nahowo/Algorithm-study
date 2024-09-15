c8cc1d5ca21e85f5070040cc7221ef773883dbb6
Nahyun Park <nahowo@naver.com>
2024-09-15 14:32:38 +0900

    [Silver I] Title: 태권왕, Time: 40 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/14562. 태권왕/태권왕.py
+++ 백준/Silver/14562. 태권왕/태권왕.py
@@ -1,23 +1,21 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
 
-def dfs(s, t, cnt):
-    global minCnt
-    if s == t:
-        minCnt = min(minCnt, cnt)
-        return
-    if s > t: # 태균이가 상대 점수를 넘어서기 시작하면 더 이상 점수가 같아질 수 없음(종료조건)
-        return
-    
-    dfs(s * 2, t + 3, cnt + 1)
-    dfs(s + 1, t, cnt + 1)
-
 def solution():
-    global minCnt
-    minCnt = sys.maxsize
     s, t = map(int, input().split())
-    dfs(s, t, 0)
-    return minCnt
+
+    # 백트래킹
+    q = [(0, s, t)]
+    while q:
+        cnt, s, t = heapq.heappop(q)
+        
+        if s == t:
+            return cnt
+        elif s > t:
+            continue
+        else:
+            heapq.heappush(q, (cnt + 1, s * 2, t + 3))
+            heapq.heappush(q, (cnt + 1, s + 1, t))
 
 t = int(input())
 for _ in range(t):
