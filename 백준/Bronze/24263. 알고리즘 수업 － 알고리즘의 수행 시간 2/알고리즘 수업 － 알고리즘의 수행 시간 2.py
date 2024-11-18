ea4e48b73f47a8bdc2df780d5dab2959dc95d365
Nahyun Park <nahowo@naver.com>
2024-11-18 16:37:28 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    n = int(input())
+    print(n) # for문은 n번만큼 반복
+    print(1) # 다항식은 f(n) = n, 즉 다항식의 최고차항의 차수는 항상 1
+    return
+
+solution()
