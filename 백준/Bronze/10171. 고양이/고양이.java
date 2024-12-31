0eaf44a8457581fda922ab7550f837e091f09191
Nahyun Park <nahowo@naver.com>
2024-12-31 13:49:37 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+public class Main {
+    static void print(Object object) {
+        System.out.println(object);
+    }
+    public static void main(String args[]) {
+        print("\\    /\\");
+        print(" )  ( ')");
+        print("(  /  )");
+        print(" \\(__)|");
+    }
+}
