39997a2bb019e865afb18513815791407acbf51e
Nahyun Park <nahowo@naver.com>
2024-12-31 13:47:55 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    static void print(Object object) {
+        System.out.println(object);
+    }
+    public static void main(String args[]) {
+        print(660);
+        print("nahowo");
+    }
+}
