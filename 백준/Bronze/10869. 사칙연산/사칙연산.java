3f2eed11950bff51864908da6a6ce2c89d9d7a7b
Nahyun Park <nahowo@naver.com>
2024-12-31 14:49:20 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static void print(Object object) {
+        System.out.println(object);
+    }
+
+    static String input() throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String str = input.readLine();
+        return str;
+    }
+    public static void main(String args[]) throws IOException{
+        String str = input();
+        int a;
+        int b;
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        print(a + b);
+        print(a - b);
+        print(a * b);
+        print((int)(a / b));
+        print(a % b);
+    }
+}
