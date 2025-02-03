30edbc2baacd59799c466edc3a551bb8fa17e77e
Nahyun Park <nahowo@naver.com>
2025-02-03 22:24:55 +0900

    [Silver I] Title: 행운의 문자열, Time: 72 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.py
@@ -12,6 +12,10 @@ def recursion(pre, l, d):
     if l == n:
         return 1
     
+    key = (pre, tuple(sorted(d.items()))) # (현재까지의 마지막 단어, 남은 단어)를 캐싱
+    if key in cache:
+        return cache[key]
+    
     for i in d.keys():
         if d[i] == 0 or i == pre:
             continue
@@ -19,13 +23,15 @@ def recursion(pre, l, d):
         answer += recursion(i, l + 1, d)
         d[i] += 1
     
+    cache[key] = answer
     return answer
 
 def solution():
-    global n
+    global n, cache
     s = list(input().rstrip())
     n = len(s)
     d = dict()
+    cache = dict()
     if len(set(s)) == n: # 전부 고유한 문자열일 경우 n!
         return factorial(n)
     for i in s:
