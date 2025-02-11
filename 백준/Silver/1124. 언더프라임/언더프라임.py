bbeba30dda50ca06b1ca4d98b0c90b2990ed81d0
Nahyun Park <nahowo@naver.com>
2025-02-11 23:15:22 +0900

    [Silver I] Title: 언더프라임, Time: 72 ms, Memory: 33976 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.py
+++ 백준/Silver/1124. 언더프라임/언더프라임.py
@@ -1,36 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-def getPrime():
-    MAX = 10 ** 5 + 1
-    prime = {i for i in range(2, MAX)}
+def prime_factorize(n):
+    global factor, prime
+    factor = [0] * (n + 1)
+    prime = [True] * (n + 1)
+    prime[0] = prime[1] = False
 
-    for i in range(2, int(MAX ** (1 / 2)) + 1):
-        if i in prime:
-            for j in range(i ** 2, MAX, i):
-                if j in prime:
-                    prime.remove(j)
-    return prime
+    for i in range(2, n + 1):
+        if prime[i]:
+            factor[i] = 1
 
-def factorize(n, prime):
-    cnt = 0
-    idx = 0
-    while n > 1:
-        if n % prime[idx] == 0:
-            cnt += 1
-            n //= prime[idx]
-        else:
-            idx += 1
-    return cnt
+            # 소수의 배수는 해당 소수를 인수로 가짐
+            for j in range(2 * i, n + 1, i):
+                prime[j] = False
+                factor[j] += 1
+            
+            # 소수의 거듭제곱수는 해당 소수를 인수로 가짐
+            sqr = i * i
+            while sqr <= n:
+                for j in range(sqr, n + 1, sqr):
+                    factor[j] += 1
+                sqr *= i
 
 def solution():
-    a, b = map(int, input().split())
-    prime = getPrime()
-    primeL = sorted(list(prime))
     answer = 0
+    a, b = map(int, input().split())
+    prime_factorize(b)
     for i in range(a, b + 1):
-        if factorize(i, primeL) in prime:
+        if prime[factor[i]]:
             answer += 1
+    
     return answer
     
 print(solution())
