dead4f683bbc0e99392e3b536793b0610fbd847d
Nahyun Park <nahowo@naver.com>
2025-02-17 11:05:45 +0900

    [level 2] Title: 올바른 괄호, Time: 9.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = True
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) == 0:
+                answer = False
+                break
+            stack.pop()
+    if len(stack) > 0:
+        answer = False
+
+    return answer
