10505d195a891c31306d1bc1f2571437ebd34464
Nahyun Park <nahowo@naver.com>
2025-02-18 21:12:05 +0900

    [level 3] Title: 정수 삼각형, Time: 38.66 ms, Memory: 14.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,11 @@
+import copy
+
+def solution(triangle):
+    n = len(triangle)
+
+    for i in range(n - 2, -1, -1):
+        for j in range(i + 1):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+    answer = triangle[0][0]
+
+    return answer
