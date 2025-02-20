3b2788203bad443b7e76b4514bc923daf02e007d
Nahyun Park <nahowo@naver.com>
2025-02-20 13:44:17 +0900

    [Gold III] Title: 줄 세우기, Time: 128 ms, Memory: 39308 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,43 +1,30 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 8)
+
+def dfs(x, graph):
+    global cnt
+    visited[x] = True
+    for nx in graph[x]:
+        if not visited[nx]:
+            dfs(nx, graph)
+    result[cnt] = x
+    cnt -= 1
 
 def solution():
+    global visited, result, cnt
     n, m = map(int, input().split())
-
-    taller = {i: [set(), 0] for i in range(1, n + 1)} # i: [{i번 학생보다 큰 학생 명단}, 자신보다 큰 학생 수]
-    tallerCnt = {i: set() for i in range(n)} # i: {자신보다 큰 학생이 i명인 학생 명단}
-    student = {i: set() for i in range(1, n + 1)} # i: {i번 학생보다 작은 학생 명단}
-
+    graph = [[] for _ in range(n + 1)]
     for _ in range(m):
         a, b = map(int, input().split())
-        taller[a][0].add(b)
-        taller[a][1] += 1
-        student[b].add(a)
-
+        graph[a].append(b)
+    visited = [False] * (n + 1)
+    result = [0] * (n)
+    cnt = n - 1
     for i in range(1, n + 1):
-        tallerCnt[taller[i][1]].add(i)
-
-    q = deque()
-    q.extend(tallerCnt[0])
-    tallerCnt[0] = set()
-    order = []
+        if not visited[i]:
+            dfs(i, graph)
     
-    while q:
-        x = q.popleft()
-        for nx in student[x]:
-            tmpCnt = taller[nx][1]
-            tallerCnt[tmpCnt].remove(nx)
-            tallerCnt[tmpCnt - 1].add(nx)
+    return result
 
-            taller[nx][0].remove(x)
-            taller[nx][1] -= 1
-        order.append(x)
-        
-        q.extend(tallerCnt[0])
-        tallerCnt[0] = set()
-    order = order[:: -1]
-
-    return order
-    
 print(*solution())
