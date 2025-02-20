a13c51cbcc6c72c86b26bdb12417fb353077d1b2
Nahyun Park <nahowo@naver.com>
2025-02-20 18:17:37 +0900

    [level 3] Title: 순위, Time: 5.79 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,33 +1,18 @@
-def dfs(x, graph, n):
-    visited = [False] * (n + 1)
-    q = [x]
-    visited[x] = True
-    cnt = 0
-    
-    while q:
-        x = q.pop()
-        for nx in graph[x]:
-            if not visited[nx]:
-                q.append(nx)
-                visited[nx] = True
-                cnt += 1
-    return cnt
-
 def solution(n, results):
     answer = 0
-    graphF = [[] for _ in range(n + 1)]
-    graphB = [[] for _ in range(n + 1)]
-    match = [[0, 0] for _ in range(n + 1)] # win, lose
+    match = [[set(), set()] for _ in range(n + 1)] # win, lose
     for a, b in results:
-        graphF[a].append(b)
-        graphB[b].append(a)
+        match[a][0].add(b)
+        match[b][1].add(a)
     
     for i in range(1, n + 1):
-        match[i][0] = dfs(i, graphF, n)
-        match[i][1] = dfs(i, graphB, n)
-    
+        for j in match[i][1]: # j > i
+            match[j][0].update(match[i][0])
+        for j in match[i][0]: # i > j
+            match[j][1].update(match[i][1])
+
     for i in range(1, n + 1):
-        if sum(match[i]) == n - 1:
+        if len(match[i][0]) + len(match[i][1]) == n - 1:
             answer += 1
     
     return answer
