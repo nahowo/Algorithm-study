c3041b7aa03b217c5888053a57091692e9c193a2
Nahyun Park <nahowo@naver.com>
2025-02-21 10:02:52 +0900

    [level 2] Title: 주식가격, Time: 36.06 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,9 +1,13 @@
 def solution(prices):
     n = len(prices)
     answer = [i for i in range(n - 1, -1, -1)]
+    stack = [] # [index, price]
     for i in range(n):
-        for j in range(i + 1, n):
-            if prices[i] > prices[j]:
-                answer[i] = j - i
+        while stack: # 현재 인덱스보다 이전 인덱스만 스택에 존재
+            if stack[-1][1] > prices[i]: # stack[-1] 인덱스 가격이 현재가격보다 큼 -> 가격 하락
+                xIdx, xVal = stack.pop()
+                answer[xIdx] = i - xIdx
+            else:
                 break
+        stack.append([i, prices[i]])
     return answer
