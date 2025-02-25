ff639f4b967c63bf329fe26bb845c418eec9f642
Nahyun Park <nahowo@naver.com>
2025-02-25 10:35:59 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -4,16 +4,13 @@ DIV = 10007
 
 def solution():
     n = int(input())
-    dp = [[0] * (10) for _ in range(n + 1)]
-    for i in range(10):
-        dp[1][i] = 1
-    
+    dp = [[0] * 10 for _ in range(n + 1)]
+    dp[1] = [1] * 10
+
     for i in range(2, n + 1):
         for j in range(10):
-            for k in range(j, 10):
-                dp[i][j] += (dp[i - 1][k]) % DIV
-                dp[i][j] %= DIV
-
+            dp[i][j] += sum(dp[i - 1][j : 10]) % DIV
+            dp[i][j] %= DIV
     return sum(dp[n]) % DIV
 
 print(solution())
