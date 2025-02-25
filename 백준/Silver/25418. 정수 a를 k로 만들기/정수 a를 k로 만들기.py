08dae9644a4ec56262fe04b8647c331c1258778a
Nahyun Park <nahowo@naver.com>
2025-02-25 23:13:21 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 248 ms, Memory: 66320 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -7,10 +7,10 @@ def solution():
     dp[a] = 0
 
     for i in range(a + 1, k + 1):
-        if dp[i - 1] + 1 < dp[i]:
-            dp[i] = dp[i - 1] + 1
-        if i % 2 == 0 and dp[i // 2] + 1 < dp[i]:
-            dp[i] = dp[i // 2] + 1
+        dp[i] = min(dp[i], dp[i - 1] + 1)
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+
     return dp[k]
 
 print(solution())
