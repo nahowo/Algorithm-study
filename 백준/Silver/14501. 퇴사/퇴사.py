40a109313fadfb2f26eb3c12368b7f77daa1e31a
Nahyun Park <nahowo@naver.com>
2025-02-27 10:55:31 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -3,20 +3,14 @@ input = sys.stdin.readline
 
 def solution():
     n = int(input())
-    counsel = [[0, 0] for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        t, p = map(int, input().split())
-        counsel[i] = [t + i, p] # 종료일, 금액
-
+    counsel = [list(map(int, input().split())) for _ in range(n)]
     dp = [0] * (n + 2)
     
-    for i in range(1, n + 2):
-        dp[i] = dp[i - 1]
-        for j in range(0, i):
-            if counsel[j][0] == i: # 종료일이 오늘인 경우
-                duration = counsel[j][0] - j
-                dp[i] = max(dp[i], dp[i - duration] + counsel[j][1])
-
+    for i in range(1, n + 1):
+        dp[i] = max(dp[i], dp[i - 1])
+        if i + counsel[i - 1][0] <= n + 1:
+            dp[i + counsel[i - 1][0]] = max(dp[i + counsel[i - 1][0]], dp[i] + counsel[i - 1][1])
+    dp[n + 1] = max(dp[n + 1], dp[n])
     return dp[n + 1]
 
 print(solution())
