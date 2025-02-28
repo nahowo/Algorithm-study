5ff98608c68e5cfa3e592261ddebd5d749238105
Nahyun Park <nahowo@naver.com>
2025-02-28 10:36:45 +0900

    [Gold II] Title: 가운데를 말해요, Time: 176 ms, Memory: 39016 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -19,31 +19,18 @@ def solution():
         else:
             heapq.heappush(big, x)
             lb += 1
-        while True:
-            if (i % 2 == 0 and ls == lb) or (i % 2 == 1 and ls == lb + 1):
-                break
-            if i % 2 == 0: # 수의 개수가 짝수: ls == lb여야 함
-                if lb > ls:
-                    while lb > ls:
-                        heapq.heappush(small, (-1) * heapq.heappop(big))
-                        ls += 1
-                        lb -= 1
-                elif ls > lb:
-                    while ls > lb:
-                        heapq.heappush(big, (-1) * heapq.heappop(small))
-                        ls -= 1
-                        lb += 1
-            else: # 수의 개수가 홀수: ls == lb + 1이여야 함
-                if lb >= ls:
-                    while lb >= ls:
-                        heapq.heappush(small, (-1) * heapq.heappop(big))
-                        ls += 1
-                        lb -= 1
-                elif ls > lb + 1:
-                    while ls > lb + 1:
-                        heapq.heappush(big, (-1) * heapq.heappop(small))
-                        ls -= 1
-                        lb += 1
+
+        if i % 2 == 0:
+            if ls == lb + 2:
+                heapq.heappush(big, (-1) * heapq.heappop(small))
+                ls -= 1
+                lb += 1
+        else:
+            if lb == ls + 1:
+                heapq.heappush(small, (-1) * heapq.heappop(big))
+                ls += 1
+                lb -= 1
+        
         print(-small[0])
 
 solution()
