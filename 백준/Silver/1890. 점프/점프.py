af795872e28e3dfc56e5dd95d63e5a66e4ef2afb
Nahyun Park <nahowo@naver.com>
2025-03-07 13:06:59 +0900

    [Silver I] Title: 점프, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -1,28 +1,20 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 5)
-
-def rec(i, j):
-    if i >= n or j >= n:
-        return 0
-    if i == n - 1 and j == n - 1:
-        return 1
-    if dp[i][j] != -1:
-        return dp[i][j]
-    
-    if jump[i][j] == 0:
-        ret = 0
-    else:
-        ret = rec(i + jump[i][j], j) + rec(i, j + jump[i][j])
-    dp[i][j] = ret
-    return ret
 
 def solution():
-    global dp, n, jump
     n = int(input())
     jump = [list(map(int, input().split())) for _ in range(n)]
-    dp = [[-1] * (n) for _ in range(n)]
+    dp = [[0] * (n) for _ in range(n)]
+    dp[0][0] = 1
+
+    for i in range(n):
+        for j in range(n):
+            if dp[i][j] and jump[i][j]:
+                if i + jump[i][j] < n:
+                    dp[i + jump[i][j]][j] += dp[i][j]
+                if j + jump[i][j] < n:
+                    dp[i][j + jump[i][j]] += dp[i][j]
     
-    return rec(0, 0)
+    return dp[n - 1][n - 1]
 
 print(solution())
