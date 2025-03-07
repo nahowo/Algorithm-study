44478570cfa676ff116b29c8bdd1d2e7ed316ba8
Nahyun Park <nahowo@naver.com>
2025-03-07 09:51:10 +0900

    [level 2] Title: 순위 검색, Time: 665.13 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,56 +1,36 @@
-def splitQuery(s):
-    tmp = s.split()
-    q = ''
-    score = int(tmp[-1])
-    for i in range(0, len(tmp) - 1, 2):
-        q += (tmp[i][0])
-    
-    return q, score
-
-def binarySearch(query, score):
-    n = classify[query][1]
-    candidate = classify[query][0]
-    key = n
-    
-    top = n - 1
-    bot = 0
-    while bot <= top:
-        mid = (top + bot) // 2
-        if candidate[mid] >= score:
-            key = mid
-            top = mid - 1
-        else:
-            bot = mid + 1
-    
-    return n - key
+import bisect
 
 def solution(info, query):
-    global classify
-    answer = []
-    classify = dict()
+    answer = [0] * len(query)
+    n = len(info)
+    # 분류 튜플 만들기
+    info_type = dict()
+    for l in ['cpp', 'java', 'python', '-']:
+        for p in ['backend', 'frontend', '-']:
+            for e in ['junior', 'senior', '-']:
+                for f in ['chicken', 'pizza', '-']:
+                    t = ' '.join([l, p, e, f])
+                    info_type[t] = []
     
-    for l in ['c', 'j', 'p', '-']:
-        for p in ['b', 'f', '-']:
-            for e in ['j', 's', '-']:
-                for f in ['c', 'p', '-']:
-                    tmp = l + p + e + f
-                    classify[tmp] = [[], 0]
+    for i in range(len(info)):
+        l, p, e, f, s = info[i].split()
+
+        for l1 in [l, '-']:
+            for p1 in [p, '-']:
+                for e1 in [e, '-']:
+                    for f1 in [f, '-']:
+                        t = ' '.join([l1, p1, e1, f1])
+                        info_type[t].append(int(s))
     
-    for i in info:
-        lang, pos, exp, food, score = i.split()
-        for l in [lang, '-']:
-            for p in [pos, '-']:
-                for e in [exp, '-']:
-                    for f in [food, '-']:
-                        tmp = l[0] + p[0] + e[0] + f[0]
-                        classify[tmp][0].append(int(score))
-                        classify[tmp][1] += 1
+    for k in info_type.keys():
+        info_type[k] = sorted(info_type[k])
     
-    for i in classify:
-        classify[i][0].sort()
+    # 컬럼별 분류
+    for i in range(len(query)):
+        l, p, e, f = query[i].split(' and ')
+        f, s = f.split()
+        s = int(s)
+        t = ' '.join([l, p, e, f])
+        answer[i] = len(info_type[t]) - bisect.bisect_left(info_type[t], s)
         
-    for s in query:
-        tmpQ, tmpS = splitQuery(s)
-        answer.append(binarySearch(tmpQ, tmpS))
-    
     return answer
