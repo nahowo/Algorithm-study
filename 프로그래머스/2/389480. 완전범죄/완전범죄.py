74fb4f516d90d41fd05097b3c488b891d875ad74
Nahyun Park <nahowo@naver.com>
2025-03-08 10:53:40 +0900

    [level 2] Title: 완전범죄, Time: 2.71 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,17 +1,15 @@
 def solution(info, n, m):
-    maxSize = 10 ** 6
+    INF = 10 ** 3
     l = len(info)
-    dp = [[maxSize] * (m) for _ in range(l + 1)]
-    dp[0][0] = 0
-    
+    dp = [[INF] * (m + 1) for _ in range(l + 1)]
+    for j in range(m):
+        dp[0][j] = 0
     for i in range(1, l + 1):
-        a, b = info[i - 1]
-        for j in range(m):
-            dp[i][j] = min(dp[i][j], dp[i - 1][j] + a)
-            if j + b < m:
-                dp[i][j + b] = min(dp[i][j + b], dp[i - 1][j])
-                
-        answer = min(dp[l])
-        if answer >= n:
-            answer = -1
-    return answer
+        for j in range(m + 1):
+            dp[i][j] = min(dp[i][j], dp[i - 1][j] + info[i - 1][0])
+            if j + info[i - 1][1] < m:
+                dp[i][j + info[i - 1][1]] = dp[i - 1][j]
+        
+    if min(dp[l]) >= n:
+        return -1
+    return min(dp[l])
