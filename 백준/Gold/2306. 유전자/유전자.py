6378ab1ffe7beb157f1458b2f5d9222ea656eea4
Nahyun Park <nahowo@naver.com>
2025-04-12 00:25:49 +0900

    [Gold III] Title: 유전자, Time: 2432 ms, Memory: 34224 KB -BaekjoonHub

--- 백준/Gold/2306. 유전자/유전자.py
+++ 백준/Gold/2306. 유전자/유전자.py
@@ -1,29 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def rec(s, e):
-    if s >= e:
-        return 0
-    if s == e:
-        return 1
-    if cache[s][e] != -1:
-        return cache[s][e]
-    
-    ret = -1
-    if (dna[s] == 'a' and dna[e] == 't') or (dna[s] == 'g' and dna[e] == 'c'):
-        ret = rec(s + 1, e - 1) + 2
-    for k in range(s, e):
-        ret = max(ret, rec(s, k) + rec(k + 1, e))
-
-    cache[s][e] = ret
-    return cache[s][e]
-
 def solution():
-    global dna, n, cache
     dna = input().rstrip()
     n = len(dna)
-    cache = [[-1] * n for _ in range(n)]
-    rec(0, n - 1)
-    return cache[0][n - 1]
+    dp = [[0] * n for _ in range(n)]
+
+    for l in range(1, n):
+        for i in range(n - l):
+            j = i + l
+            if (dna[i] == 'a' and dna[j] == 't') or (dna[i] == 'g' and dna[j] == 'c'):
+                dp[i][j] = dp[i + 1][j - 1] + 2
+            for k in range(i, j):
+                dp[i][j] = max(dp[i][j], dp[i][k] + dp[k + 1][j])
+
+    return dp[0][n - 1]
     
 print(solution())
