28248771f792a47d8fad0fe1bbadd3321306304f
Nahyun Park <nahowo@naver.com>
2025-05-19 23:29:04 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int n = num_list.length;
+        int[] answer = new int[n + 1];
+        for (int i = 0; i < n; i ++) {
+            answer[i] = num_list[i];
+        }
+        
+        answer[n] = (answer[n - 1] > answer[n - 2]) ? answer[n - 1] - answer[n - 2] : answer[n - 1] * 2;
+        return answer;
+    }
+}
