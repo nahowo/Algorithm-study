38dc480addca13777f61091c0ba7843a803956ce
Nahyun Park <nahowo@naver.com>
2025-07-05 10:17:52 +0900

    [level 0] Title: 옹알이 (1), Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,22 @@
+function solution(babbling) {
+    var answer = 0;
+    for (let b of babbling) {
+        answer += check(b);
+    }
+    return answer;
+}
+
+function check(word) {
+    let l = word.length;
+    word = word.replace(/aya/, " ");
+    word = word.replace(/ye/, " ");
+    word = word.replace(/woo/, " ");
+    word = word.replace(/ma/, " ");
+    
+    for (let w of word) {
+        if (w != " ") {
+            return 0;
+        }
+    }
+    return 1;
+}
