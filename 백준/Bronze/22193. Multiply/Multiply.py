c3cd5e6f70c49adc485e01ba3673a9efca91edc8
Nahyun Park <nahowo@naver.com>
2025-10-08 12:53:20 +0900

    [Bronze V] Title: Multiply, Time: 144 ms, Memory: 39276 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.py
+++ 백준/Bronze/22193. Multiply/Multiply.py
@@ -5,16 +5,6 @@ def solution():
     n, m = map(int, input().split())
     a = int(input())
     b = int(input())
-    answer = 0
-
-    for i in range(n):
-        ti = a % 10
-        a = a // 10
-        tmpb = b
-        for j in range(m):
-            tj = tmpb % 10
-            tmpb = tmpb // 10
-            answer += (ti * tj) * 10 ** (i + j)
-    return answer
+    return a * b
 
 print(solution())
