430aff14911f5611038d456f1850b5716f323aaa
Nahyun Park <nahowo@naver.com>
2026-01-27 16:42:02 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] tmp = br.readLine().split(" ");
+        int n = Integer.parseInt(tmp[0]);
+        int m = Integer.parseInt(tmp[1]);
+        if (m == 1 | m == 2) {
+            System.out.println("NEWBIE!");
+        } else if (m <= n) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TLE!");
+        }
+    }
+}
