f295b4be901b14b0c5a580948c2e62c94d9aabf3
Nahyun Park <nahowo@naver.com>
2026-02-14 20:56:18 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,14 +1,20 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-t=int(input())
-dp=[0]*12
-dp[1]=1
-dp[2]=2
-dp[3]=4
-for i in range(4,12):
-    dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+def solution():
+    global dp
+    dp = [0] * 12
+    dp[1], dp[2], dp[3] = 1, 1, 1
 
-for i in range(t):
-    tmp=int(input())
-    print(dp[tmp])
+    for i in range(2, 12):
+        dp[i] += dp[i - 1]
+        if i > 2:
+            dp[i] += dp[i - 2]
+        if i > 3:
+            dp[i] += dp[i - 3]
+
+solution()
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
