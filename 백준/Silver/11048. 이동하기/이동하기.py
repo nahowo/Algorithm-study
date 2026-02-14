d59ec33423aaecbcb695dda7532b9f3c334e3f05
Nahyun Park <nahowo@naver.com>
2026-02-14 22:54:36 +0900

    [Silver II] Title: 이동하기, Time: 308 ms, Memory: 80524 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -4,11 +4,17 @@ input = sys.stdin.readline
 def solution():
     n, m = map(int, input().split())
     candy = [list(map(int, input().split())) for _ in range(n)]
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp = [[0] * m for _ in range(n)]
+    dp[0][0] = candy[0][0]
+    for i in range(1, n):
+        dp[i][0] = candy[i][0] + dp[i - 1][0]
+    for j in range(1, m):
+        dp[0][j] = candy[0][j] + dp[0][j - 1]
+    
+    for i in range(1, n):
+        for j in range(1, m):
+            dp[i][j] = candy[i][j] + max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
 
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            dp[i][j] = candy[i - 1][j - 1] + max([dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]])
-    return dp[n][m]
+    return dp[n - 1][m - 1]
 
 print(solution())
