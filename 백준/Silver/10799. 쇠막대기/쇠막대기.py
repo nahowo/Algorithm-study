cebe4164b56ce6fe3f1a1908246f16f3dd5fda6f
Nahyun Park <nahowo@naver.com>
2026-02-14 13:36:52 +0900

    [Silver II] Title: 쇠막대기, Time: 48 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -3,22 +3,20 @@ input = sys.stdin.readline
 
 def solution():
     s = input().rstrip()
+    s = s.replace("()", "|")
     n = len(s)
     answer = 0
 
     q = 0
-    i = 0
-    while i < n:
-        if i < n - 1 and s[i] == "(" and s[i + 1] == ")":
-            answer += q # 현재 레이저 경로에 있는 막대 개수
-            i += 2
+    for i in range(n):
+        if s[i] == "|":
+            answer += q
         else:
             if s[i] == "(":
                 q += 1
             else:
                 q -= 1
-                answer += 1 # 막대 끝
-            i += 1
+                answer += 1
     return answer
     
 print(solution())
