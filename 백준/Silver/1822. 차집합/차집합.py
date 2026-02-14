4fbbbf935a8084ff0c00e9ca25e1beb8dfc028ac
Nahyun Park <nahowo@naver.com>
2026-02-14 18:09:59 +0900

    [Silver IV] Title: 차집합, Time: 500 ms, Memory: 130208 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -1,29 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-def binarySearch(target, b):
-    start, end = 0, len(b) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-        if b[mid] == target:
-            return True
-        elif b[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return False
-
 def solution():
     answer = []
     na, nb = map(int, input().split())
     a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
+    b = set(map(int, input().split()))
     a.sort()
-    b.sort()
 
     for i in a:
-        if not binarySearch(i, b):
+        if i not in b:
             answer.append(i)
     return str(len(answer)) + "\n" + " ".join(map(str, answer))
     
